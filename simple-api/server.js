11aed341a1ee446c2a3733ecb1bb393eb8b03a21
jvictorfarias <victorfarias.new@gmail.com>
2020-07-15 17:58:42 -0300

    creating dynamic templates

--- simple-api/server.js
+++ simple-api/server.js
@@ -2,19 +2,75 @@ const http = require('http')
 const { resolve } = require('path')
 const fs = require('fs')
 
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName)
+  output = output.replace(/{%PRODUCT_IMAGE%}/g, product.image)
+  output = output.replace(/{%PRODUCT_FROM%}/g, product.from)
+  output = output.replace(/{%PRODUCT_ID%}/g, product.id)
+  output = output.replace(/{%PRODUCT_NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%PRODUCT_DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%PRODUCT_QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%PRODUCT_PRICE%}/g, product.price)
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+  return output
+}
+
 const server = http.createServer((request, response) => {
   const slug = request.url
 
+  // Overview
+
   if (slug === '/overview' || slug === '/') {
-    response.writeHead(200, {
-      'Content-type': 'text/html'
-    })
-    response.end('<h1>Overview Page</h1>')
+    fs.readFile(
+      resolve(__dirname, 'templates', 'overview.html'),
+      { encoding: 'utf-8' },
+      (err, overviewTemplate) => {
+        if (err) return response.end('error')
+
+        fs.readFile(
+          resolve(__dirname, 'data', 'data.json'),
+          { encoding: 'utf-8' },
+          (err, data) => {
+            if (err) return response.end('error')
+
+            fs.readFile(
+              resolve(__dirname, 'templates', 'card.html'),
+              { encoding: 'utf-8' },
+              (err, cardTemplate) => {
+                if (err) return response.end('error')
+
+                const dataFormatted = JSON.parse(data)
+                const cardsHtml = dataFormatted.map(el =>
+                  replaceTemplate(cardTemplate, el)
+                )
+
+                overviewTemplate = overviewTemplate.replace(
+                  /{%PRODUCT_CARD%}/g,
+                  cardsHtml
+                )
+
+                response.writeHead(200, {
+                  'Content-type': 'text/html'
+                })
+                response.end(overviewTemplate)
+              }
+            )
+          }
+        )
+      }
+    )
+
+    // Product page
   } else if (slug === '/product') {
     response.writeHead(200, {
       'Content-type': 'text/html'
     })
     response.end('<h1>Product details</h1>')
+
+    // API
   } else if (slug === '/api') {
     fs.readFile(
       resolve(__dirname, 'data', 'data.json'),
@@ -31,6 +87,8 @@ const server = http.createServer((request, response) => {
         response.end(data)
       }
     )
+
+    // Not found
   } else {
     response.writeHead(404, {
       'Content-type': 'text/html'
